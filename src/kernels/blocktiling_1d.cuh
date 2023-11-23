65ca169154c2cb04e0565f7e88db5fa6ace13bdc
andsonder <changlu@keter.top>
2023-11-23 11:12:58 +0000

    change vars' name

--- src/kernels/blocktiling_1d.cuh
+++ src/kernels/blocktiling_1d.cuh
@@ -7,32 +7,32 @@ template <const int BM, const int BN, const int BK, const int TM>
 __global__ void sgemm_blocktiling_1d_kernel(const float *A, const float *B, float *C, int M, int N, int K)
 {
     // the output block that we want to compute in this threadblock
-    const uint bx = blockIdx.y;
-    const uint by = blockIdx.x;
+    const uint c_row = blockIdx.y;
+    const uint c_col = blockIdx.x;
 
     // allocate shared memory for the input and output submatrices
     __shared__ float A_shared[BM * BK];
     __shared__ float B_shared[BK * BN];
 
     // the inner row & col that we're accessing in this thread
-    const uint thread_x = threadIdx.x % BN;
-    const uint thread_y = threadIdx.x / BN;
+    const uint thread_row = threadIdx.x / BN;
+    const uint thread_col = threadIdx.x % BN;
 
     // advance pointers to the starting positions
-    A += bx * BM * K;
-    B += by * BN;
-    C += bx * BM * N + by * BN;
+    A += c_row * BM * K;
+    B += c_col * BN;
+    C += c_row * BM * N + c_col * BN;
 
-    int global_x = bx * BM * K;
-    int global_y = by * BN;
+    int global_m_pos = c_row * BM * K;
+    int global_n_pos = c_col * BN;
 
     assert(BM * BK == blockDim.x);
     assert(BN * BK == blockDim.x);
 
-    const uint A_inner_x = threadIdx.x % BK; // warp-level GMEM coalescing
-    const uint A_inner_y = threadIdx.x / BK;
-    const uint B_inner_x = threadIdx.x % BN; // warp-level GMEM coalescing
-    const uint B_inner_y = threadIdx.x / BN;
+    const uint A_inner_row = threadIdx.x / BK; // warp-level GMEM coalescing
+    const uint A_inner_col = threadIdx.x % BK;
+    const uint B_inner_row = threadIdx.x / BN; // warp-level GMEM coalescing
+    const uint B_inner_col = threadIdx.x % BN; 
 
     // allocate thread-local cache for results in registerfile
     float thread_results[TM] = {0.0};
@@ -41,8 +41,8 @@ __global__ void sgemm_blocktiling_1d_kernel(const float *A, const float *B, floa
     for (uint bk_idx = 0; bk_idx < K; bk_idx += BK)
     {
         // load the next block of the input matrices into shared memory
-        A_shared[A_inner_y * BK + A_inner_x] = (global_x + A_inner_y * K + A_inner_x < M * K) ? A[A_inner_y * K + A_inner_x] : 0.0f;
-        B_shared[B_inner_y * BN + B_inner_x] = (global_y + B_inner_y * N + B_inner_x < N * K) ? B[B_inner_y * N + B_inner_x] : 0.0f;
+        A_shared[A_inner_row * BK + A_inner_col] = (global_m_pos + A_inner_row * K + A_inner_col < M * K) ? A[A_inner_row * K + A_inner_col] : 0.0f;
+        B_shared[B_inner_row * BN + B_inner_col] = (global_n_pos + B_inner_row * N + B_inner_col < N * K) ? B[B_inner_row * N + B_inner_col] : 0.0f;
 
         // wait for all threads to finish loading
         __syncthreads();
@@ -50,19 +50,19 @@ __global__ void sgemm_blocktiling_1d_kernel(const float *A, const float *B, floa
         // advance the pointers
         A += BK;
         B += BK * N;
-        global_x += BK;
-        global_y += BK * N;
+        global_m_pos += BK;
+        global_n_pos += BK * N;
 
         // compute the partial sum
         for (uint dot_idx = 0; dot_idx < BK; dot_idx++)
         {
             // we make the dotproduct loop the outside loop, which facilitates
             // reuse of the Bs entry, which we can cache in a tmp var.
-            float tmp_b = B_shared[dot_idx * BN + thread_x];
+            float tmp_b = B_shared[dot_idx * BN + thread_col];
             for (uint res_idx = 0; res_idx < TM; res_idx++)
             {
                 // (threadRow * TM + resIdx) * BK + dotIdx
-                thread_results[res_idx] += A_shared[(thread_y * TM + res_idx) * BK + dot_idx] * tmp_b;
+                thread_results[res_idx] += A_shared[(thread_row * TM + res_idx) * BK + dot_idx] * tmp_b;
             }
         }
 
@@ -72,9 +72,9 @@ __global__ void sgemm_blocktiling_1d_kernel(const float *A, const float *B, floa
 
     for (uint res_idx = 0; res_idx < TM; res_idx++)
     {
-        if (bx * BM + thread_y * TM + res_idx < M && by * BN + thread_x < N)
+        if (c_row * BM + thread_row * TM + res_idx < M && c_col * BN + thread_col < N)
         {
-            C[(thread_y * TM + res_idx) * N + thread_x] = thread_results[res_idx];
+            C[(thread_row * TM + res_idx) * N + thread_col] = thread_results[res_idx];
         }
     }
 }
--- src/kernels/shared_mem.cuh
+++ src/kernels/shared_mem.cuh
@@ -7,28 +7,28 @@ template <const int BLOCKSIZE>
 __global__ void sgemm_shared_mem_kernel(const float *A, const float *B, float *C, int M, int N, int K)
 {
     // the output block that we want to compute in this threadblock
-    const uint bx = blockIdx.x;
-    const uint by = blockIdx.y;
+    const uint c_row = blockIdx.x;
+    const uint c_col = blockIdx.y;
 
     // allocate shared memory for the input and output submatrices
     __shared__ float A_shared[BLOCKSIZE * BLOCKSIZE];
     __shared__ float B_shared[BLOCKSIZE * BLOCKSIZE];
 
     // the inner row & col that we're accessing in this thread
-    const uint thread_x = threadIdx.x % BLOCKSIZE;
-    const uint thread_y = threadIdx.x / BLOCKSIZE;
+    const uint thread_row = threadIdx.x / BLOCKSIZE;
+    const uint thread_col = threadIdx.x % BLOCKSIZE;
 
     // advance pointers to the starting positions
-    A += bx * BLOCKSIZE * K;
-    B += by * BLOCKSIZE;
-    C += bx * BLOCKSIZE * N + by * BLOCKSIZE;
+    A += c_row * BLOCKSIZE * K;
+    B += c_col * BLOCKSIZE;
+    C += c_row * BLOCKSIZE * N + c_col * BLOCKSIZE;
 
     float tmp = 0.0f;
     for (int i = 0;i < K;i += BLOCKSIZE)
     {
         // load the next block of the input matrices into shared memory
-        A_shared[thread_x * BLOCKSIZE + thread_y] = (bx * BLOCKSIZE + thread_x < M && i + thread_y < K) ? A[thread_x * K + thread_y] : 0.0f;
-        B_shared[thread_x * BLOCKSIZE + thread_y] = (by * BLOCKSIZE + thread_y < N && i + thread_x < K) ? B[thread_x * N + thread_y] : 0.0f;
+        A_shared[thread_col * BLOCKSIZE + thread_row] = (c_row * BLOCKSIZE + thread_col < M && i + thread_row < K) ? A[thread_col * K + thread_row] : 0.0f;
+        B_shared[thread_col * BLOCKSIZE + thread_row] = (c_col * BLOCKSIZE + thread_row < N && i + thread_col < K) ? B[thread_col * N + thread_row] : 0.0f;
 
         // wait for all threads to finish loading
         __syncthreads();
@@ -36,7 +36,7 @@ __global__ void sgemm_shared_mem_kernel(const float *A, const float *B, float *C
         // compute the partial sum
         for (int j = 0; j < BLOCKSIZE; j++)
         {
-            tmp += A_shared[thread_x * BLOCKSIZE + j] * B_shared[j * BLOCKSIZE + thread_y];
+            tmp += A_shared[thread_col * BLOCKSIZE + j] * B_shared[j * BLOCKSIZE + thread_row];
         }
 
         // wait for all threads to finish computing
@@ -47,7 +47,7 @@ __global__ void sgemm_shared_mem_kernel(const float *A, const float *B, float *C
         B += BLOCKSIZE * N;
     }
 
-    if (bx * BLOCKSIZE + thread_x < M && by * BLOCKSIZE + thread_y < N) {
-        C[thread_x * N + thread_y] = tmp;
+    if (c_row * BLOCKSIZE + thread_col < M && c_col * BLOCKSIZE + thread_row < N) {
+        C[thread_col * N + thread_row] = tmp;
     }
 }
